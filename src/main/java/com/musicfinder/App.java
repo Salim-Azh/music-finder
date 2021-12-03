43b4f31e613ceeefb3e33ee19b7a27f383ad8db9
Baptiste Mouton <baptiste.mouton@etu.umontpellier.fr>
2021-12-03 00:48:56 -0500

    Connection to the database

--- src/main/java/com/musicfinder/App.java
+++ src/main/java/com/musicfinder/App.java
@@ -1,9 +1,15 @@
 package com.musicfinder;
 
+import com.mongodb.MongoClient;
+import com.musicfinder.persistence.ConnectionMongoDB;
+
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        ConnectionMongoDB.getInstance().connection();
+        MongoClient mc = ConnectionMongoDB.getInstance().getClient();
+        System.out.println(mc.getCredential());
     }
 }
